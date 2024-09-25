09b5ee748b1f4f7c8cd7dc97cbfcb6789990b861
walgren <43451710+walgren@users.noreply.github.com>
2024-09-25 14:05:39 -0600

    Add try-except to allow for non-latex distributions.

--- calibration/create_calibration_parameters.py
+++ calibration/create_calibration_parameters.py
@@ -1862,48 +1862,30 @@ class CalibrationParametersWidget(QtWidgets.QWidget):
         '''
         rc["font.serif"] = "Palatino Linotype"
         rc["font.family"] = "serif"
-        rc["text.usetex"] = True
+        try:
+            rc["text.usetex"] = True
+        except:
+            print('Rending mathematical symbols without LaTeX')
 
         dpi = 125
         fig = Figure(figsize=(width/dpi, height/dpi), dpi=dpi)
-        # fig = Figure(dpi=dpi)
         canvas = FigureCanvas(fig)
         ax = fig.gca()
-
-
         ax.text(0.0,0.0,text,va='center',ha='center',fontsize=10)
 
-        #ax.text(0.05, -0.02, r'{}'.format(text), ha="right")
         ax.axis('off')
         ax.margins(0)
         ax.patch.set_facecolor('none')
-
-        #fig.patch.set_facecolor("black")
         fig.patch.set_facecolor('none')
-        # fig.tight_layout(pad=0.0)
-
 
         canvas.draw()
-
         canvas.print_figure("latex.png",facecolor=fig.get_facecolor())
-
         pixmap = QPixmap('latex.png')
-        # size= pixmap.size()
-        # pixmap = pixmap.scaled(size * 2)
+
         label = QLabel(self)
         label.setPixmap(pixmap)
-        # label.setScaledContents(True)
         import os
         os.remove('latex.png')
-        #sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        #sizePolicy.setHorizontalStretch(0)
-        #sizePolicy.setVerticalStretch(0)
-
-        #label.setSizePolicy(sizePolicy)
-        #label.setMaximumSize(QSize(width, height))
         label.setMinimumSize(QSize(width, height))
-        #label.setAlignment(QtCore.Qt.AlignLeft)
-        #label.setAlignment(QtCore.Qt.AlignVCenter)
-
 
         return label
